66ed16d18b7bd175528ae05aded4132e15deec77
Fadel <fadel.g.heji@gmail.com>
2022-07-31 06:24:34 -0400

    commit tree 35ca2d0871d3c510bdc3ee5010db70e823463fee

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+  attr_accessor :side_1, :side_2, :side_3
+
+  def initialize side_1, side_2, side_3
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def kind
+      sum_1 = side_2 + side_3
+      sum_2 = side_1 + side_3
+      sum_3 = side_1 + side_2
+
+    if !(sum_1 > side_1 && sum_2 > side_2 && sum_3 > side_3)
+        raise TriangleError
+    elsif side_1 == side_2 && side_2 == side_3
+     return :equilateral
+    elsif side_1 == side_2 || side_1 == side_3 || side_2 == side_3
+      return :isosceles
+    else
+      return  :scalene
+    end
+  end
+
+
+  class TriangleError < StandardError
+    "The side lengths are not valid for a triangle"
+  end
 end
